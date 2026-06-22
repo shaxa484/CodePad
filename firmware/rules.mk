9a4f87244cbfb2d702dfc2698a1ad025475ee6f9
Do'ngboyev Shaxriyor <96856731+shaxa484@users.noreply.github.com>
2026-06-22 17:16:04 +0500

    Update configurations

--- firmware/rules.mk
+++ firmware/rules.mk
@@ -1,15 +1,6 @@
-# Hardware drivers
-ENCODER_DRIVER = quad_decoder
-I2C_DRIVER = I2CD1
 
-# Assign Encoder Hardware Pins
-ENCODER_PADS_A = GP1
-ENCODER_PADS_B = GP2
-
-# Assign I2C Pins for Screen
-I2C1_SDA_PIN = GP3
-I2C1_SCL_PIN = GP21
-
+OLED_ENABLE = yes
+OLED_DRIVER = ssd1306
 ENCODER_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
 
