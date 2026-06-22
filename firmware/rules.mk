d42cb0759eff6ddcfbc7feeff1ceec6c648be17b
Do'ngboyev Shaxriyor <96856731+shaxa484@users.noreply.github.com>
2026-06-22 14:31:21 +0500

    Add initial hardware driver configuration

---
+++ firmware/rules.mk
@@ -0,0 +1,16 @@
+# Hardware drivers
+ENCODER_DRIVER = quad_decoder
+I2C_DRIVER = I2CD1
+
+# Assign Encoder Hardware Pins
+ENCODER_PADS_A = GP1
+ENCODER_PADS_B = GP2
+
+# Assign I2C Pins for Screen
+I2C1_SDA_PIN = GP3
+I2C1_SCL_PIN = GP21
+
+ENCODER_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+
+LTO_ENABLE = yes
