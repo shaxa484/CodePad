2608ea480e7a7603c6a13c87b3c33aaa39466843
Do'ngboyev Shaxriyor <96856731+shaxa484@users.noreply.github.com>
2026-06-22 16:58:10 +0500

    Add configuration definitions for firmware

---
+++ firmware/config.h
@@ -0,0 +1,11 @@
+#define MANUFACTURER    Shaxriyor
+#define PRODUCT         CodePad
+
+#define DIRECT_PINS { \
+    { D0, D1, D2 }, \
+    { D3, D4, D5 }  \
+}
+
+#define ENCODERS_PAD_A { D6 }
+#define ENCODERS_PAD_B { D7 }
+#define ENCODER_RESOLUTION 4
